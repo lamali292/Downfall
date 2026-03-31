fb4f93e1d02ba2d9943a750f6728863916387f19
Laurin Liebhart <l.liebhart@tu-braunschweig.de>
2026-03-31 23:55:37 +0200

    Remove .import files from tracking

---
+++ Code/Cards/Awakened/Rare/RealityRift.cs
@@ -0,0 +1,35 @@
+﻿using BaseLib.Utils;
+using Downfall.Code.Abstract;
+using Downfall.Code.Cards.CardModels;
+using Downfall.Code.Commands;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+
+namespace Downfall.Code.Cards.Awakened.Rare;
+
+
+[Pool(typeof(AwakenedCardPool))]
+public class RealityRift : AwakenedCardModel
+{
+    public RealityRift() : base(0, CardType.Skill, CardRarity.Rare, TargetType.Self)
+    {
+        WithKeywords(CardKeyword.Exhaust);
+    }
+
+
+    protected override async Task PlayEffect(PlayerChoiceContext ctx, CardPlay cardPlay)
+    {
+        var card = await CommonActions.SelectSingleCard(this, SelectionScreenPrompt, ctx, PileType.Draw);
+        if (card == null) return;
+        await CardCmd.AutoPlay(ctx, card, null);
+        await DownfallCardCmd.GiveCards<Void>(Owner, PileType.Draw,  card.EnergyCost.GetResolved(), CardPilePosition.Random);
+    }
+
+
+    protected override void OnUpgrade()
+    {
+        RemoveKeyword(CardKeyword.Exhaust);
+    }
+   
+}
