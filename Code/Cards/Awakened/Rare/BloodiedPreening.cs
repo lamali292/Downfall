8a6af165805fed19f8ccbc008b03cfb97acc5e36
Laurin Liebhart <l.liebhart@tu-braunschweig.de>
2026-04-01 02:14:33 +0200

    added important import files

---
+++ Code/Cards/Awakened/Rare/BloodiedPreening.cs
@@ -0,0 +1,36 @@
+﻿using BaseLib.Utils;
+using Downfall.Code.Abstract;
+using Downfall.Code.Cards.Awakened.Token;
+using Downfall.Code.Cards.CardModels;
+using Downfall.Code.Powers.Awakened;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Potions;
+using MegaCrit.Sts2.Core.Models.Powers;
+
+namespace Downfall.Code.Cards.Awakened.Rare;
+
+[Pool(typeof(AwakenedCardPool))]
+public class BloodiedPreening : AwakenedCardModel
+{
+    public BloodiedPreening() : base(0, CardType.Power, CardRarity.Rare, TargetType.None)
+    {
+        WithTip(typeof(StrengthPower));
+        WithTip(typeof(PlumeJab));
+    }
+
+
+    protected override async Task PlayEffect(PlayerChoiceContext ctx, CardPlay cardPlay)
+    {
+        await CommonActions.ApplySelf<StrengthPower>(this, -2);
+        await CommonActions.ApplySelf<BloodiedPreeningPower>(this, 1);
+    }
+
+    protected override void OnUpgrade()
+    {
+        AddKeyword(CardKeyword.Innate);
+    }
+}
---
+++ Code/Powers/Awakened/BloodiedPreeningPower.cs
@@ -0,0 +1,34 @@
+﻿using Downfall.Code.Abstract;
+using Downfall.Code.Cards.Awakened.Token;
+using Downfall.Code.Commands;
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Players;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+
+namespace Downfall.Code.Powers.Awakened;
+
+public class BloodiedPreeningPower : AwakenedPowerModel
+{
+    public override PowerType Type => PowerType.Buff;
+    public override PowerStackType StackType => PowerStackType.Counter;
+
+    protected override IEnumerable<IHoverTip> ExtraHoverTips =>
+    [
+        HoverTipFactory.FromCard<PlumeJab>()
+    ];
+    
+    
+    public override async Task BeforeHandDraw(
+        Player player,
+        PlayerChoiceContext choiceContext,
+        CombatState combatState)
+    {
+        if (player.Creature != Owner) return;
+        await DownfallCardCmd.GiveCards<PlumeJab>(player, PileType.Hand, Amount, animationTime: 0.1f);
+        Flash();
+    }
+}
