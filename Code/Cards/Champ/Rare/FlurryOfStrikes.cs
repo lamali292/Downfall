4da5fddb665b2820f5e074357f2fe92e7526453e
lamali <l.liebhart@tu-braunschweig.de>
2026-04-10 01:09:22 +0200

    Implement FlurryOfStrikes stance change handling and add Godot spine extension files to .gitignore.

--- Code/Cards/Champ/Rare/FlurryOfStrikes.cs
+++ Code/Cards/Champ/Rare/FlurryOfStrikes.cs
@@ -1,13 +1,16 @@
 using BaseLib.Utils;
 using Downfall.Code.Abstract;
 using Downfall.Code.Cards.CardModels;
+using Downfall.Code.Core.Champ;
+using Downfall.Code.Events;
 using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Players;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 
 namespace Downfall.Code.Cards.Champ.Rare;
 
 [Pool(typeof(ChampCardPool))]
-public class FlurryOfStrikes : ChampCardModel
+public class FlurryOfStrikes : ChampCardModel, IOnStanceChange
 {
     public FlurryOfStrikes() : base(1, CardType.Attack, CardRarity.Rare, TargetType.AnyEnemy)
     {
@@ -21,5 +24,10 @@ public class FlurryOfStrikes : ChampCardModel
 
     protected override void OnUpgrade()
     {
+    }
+
+    public async Task OnStanceChange(PlayerChoiceContext ctx, Player player, ChampStanceModel oldStance, ChampStanceModel newStance)
+    {
+      
     }
 }
