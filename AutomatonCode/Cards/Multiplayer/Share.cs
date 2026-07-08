47f94abf6f04b09638e1f2e0a82d56206137ff13
lamali <lamali292@gmail.com>
2026-07-08 04:48:07 +0200

    Add new multiplayer cards: Share, Export, and Bluescreen. Implement `SharePower` functionality and update localized descriptions. Buff and rework Uptick card.

---
+++ AutomatonCode/Cards/Multiplayer/Share.cs
@@ -0,0 +1,29 @@
+﻿using Automaton.AutomatonCode.Core;
+using Automaton.AutomatonCode.Powers;
+using BaseLib.Utils;
+using Downfall.DownfallCode.Artists;
+using Downfall.DownfallCode.CustomEnums;
+using MegaCrit.Sts2.Core.CardSelection;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+
+namespace Automaton.AutomatonCode.Cards.Multiplayer;
+
+[Pool(typeof(AutomatonCardPool))]
+public class Share : AutomatonCardModel
+{
+    public Share() : base(1, CardType.Power, CardRarity.Uncommon, TargetType.Self)
+    {
+        this.WithPower<SharePower>(3, 1, false);
+        WithTip(StaticHoverTip.Block);
+    }
+    
+    public override CardMultiplayerConstraint MultiplayerConstraint => CardMultiplayerConstraint.MultiplayerOnly;
+
+    protected override async Task OnPlayInternal(PlayerChoiceContext ctx, CardPlay cardPlay)
+    {
+        await CommonActions.ApplySelf<SharePower>(ctx, this);
+    }
+}
---
+++ AutomatonCode/Powers/SharePower.cs
@@ -0,0 +1,29 @@
+﻿using Automaton.AutomatonCode.Cards.Token;
+using Automaton.AutomatonCode.Core;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace Automaton.AutomatonCode.Powers;
+
+public class SharePower : AutomatonPowerModel
+{
+    protected override async Task AfterBlockGained(
+        PlayerChoiceContext ctx, 
+        Creature creature, 
+        decimal amount, 
+        ValueProp props,
+        CardModel? cardSource)
+    {
+        if (creature != Owner || creature.Player == null || cardSource is not FunctionCard)
+            return;
+        var teammate =
+            creature.Player.RunState.Rng.CombatTargets.NextItem(CombatState.GetTeammatesOf(Owner)
+                .Where(e => e.IsAlive));
+        if (teammate == null) return;
+        await CreatureCmd.GainBlock(teammate, Amount, ValueProp.Unpowered, null);
+    
+    }
+}
