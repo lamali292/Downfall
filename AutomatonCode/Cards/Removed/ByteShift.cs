1429878103c92884bd4f26b84b9d131d653c9af7
lamali <lamali292@gmail.com>
2026-07-22 00:03:58 +0200

    Remove obsolete Automaton cards (`ByteShift`, `Debug`, `Decompile`, `GetLatest`), update localization for renamed cards, and clean up associated assets.

--- AutomatonCode/Cards/Removed/ByteShift.cs
+++
@@ -1,9 +0,0 @@
-﻿using BaseLib.Utils;
-using MegaCrit.Sts2.Core.Entities.Cards;
-using MegaCrit.Sts2.Core.Models.CardPools;
-
-namespace Automaton.AutomatonCode.Cards.Removed;
-
-[Obsolete]
-[Pool(typeof(TokenCardPool))]
-public class ByteShift() : AutomatonCardModel(0, CardType.Skill, CardRarity.Token, TargetType.Self);
--- AutomatonCode/Cards/Removed/GetLatest.cs
+++
@@ -1,9 +0,0 @@
-﻿using Automaton.AutomatonCode.Core;
-using BaseLib.Utils;
-using MegaCrit.Sts2.Core.Entities.Cards;
-
-namespace Automaton.AutomatonCode.Cards.Removed;
-
-[Obsolete]
-[Pool(typeof(AutomatonCardPool))]
-public class GetLatest() : AutomatonCardModel(1, CardType.Skill, CardRarity.Uncommon, TargetType.Self, false, false);
