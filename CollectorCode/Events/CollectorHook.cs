62c910f16a4247456df19702b605de8a16ca0d3d
lamali <lamali292@gmail.com>
2026-07-24 00:30:37 +0200

    Refactor hooks in `GremlinsHook` and `CollectorHook` to use `HookUtils` instead of `DownfallHook`. Update localization for `SNECKO-MULLIGAN_POWER`.

--- CollectorCode/Events/CollectorHook.cs
+++ CollectorCode/Events/CollectorHook.cs
@@ -1,4 +1,5 @@
-﻿using Downfall.DownfallCode.Events;
+﻿using BaseLib.Utils;
+using Downfall.DownfallCode.Events;
 using MegaCrit.Sts2.Core.Combat;
 using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.Entities.Players;
@@ -11,22 +12,22 @@ public static class CollectorHook
 {
     public static int ModifyCollectorDoomDamage(ICombatState cs, Creature creature, int baseAmount)
     {
-        return DownfallHook.Aggregate<IModifyCollectorDoomDamage, int>(cs, baseAmount,
+        return HookUtils.Aggregate<IModifyCollectorDoomDamage, int>(cs, baseAmount,
             (m, current) => m.ModifyCollectorDoomDamage(creature, current));
     }
 
     public static bool PreventDoomRemoval(ICombatState cs, Creature creature)
     {
-        return DownfallHook.Any<IPreventDoomRemoval>(cs, m => m.PreventDoomRemoval(creature));
+        return HookUtils.Any<IPreventDoomRemoval>(cs, m => m.PreventDoomRemoval(creature));
     }
 
     public static bool PreventCollectedDraw(ICombatState cs, Player player)
     {
-        return DownfallHook.Any<IPreventCollectedDraw>(cs, m => m.PreventCollectedDraw(player));
+        return HookUtils.Any<IPreventCollectedDraw>(cs, m => m.PreventCollectedDraw(player));
     }
 
     public static Task OnPyre(ICombatState cs, PlayerChoiceContext ctx, CardModel card, CardModel pyred)
     {
-        return DownfallHook.Dispatch<IOnPyre>(cs, ctx, m => m.OnPyre(ctx, card, pyred));
+        return HookUtils.Dispatch<IOnPyre>(cs, ctx, m => m.OnPyre(ctx, card, pyred));
     }
 }
--- GremlinsCode/Events/GremlinsHook.cs
+++ GremlinsCode/Events/GremlinsHook.cs
@@ -1,4 +1,5 @@
-﻿using Downfall.DownfallCode.Events;
+﻿using BaseLib.Utils;
+using Downfall.DownfallCode.Events;
 using Gremlins.GremlinsCode.Core;
 using Gremlins.GremlinsCode.Powers;
 using MegaCrit.Sts2.Core.Combat;
@@ -13,25 +14,25 @@ public class GremlinsHook
     public static Task AfterGremlinSwap(ICombatState cs, PlayerChoiceContext ctx, Player player,
         GremlinSwapType gremlinSwapType)
     {
-        return DownfallHook.Dispatch<IAfterGremlinSwap>(cs, ctx,
+        return HookUtils.Dispatch<IAfterGremlinSwap>(cs, ctx,
             m => m.AfterGremlinSwap(ctx, player, gremlinSwapType));
     }
 
 
     public static decimal ModifyWizExtraDamage(WizPower wizPower, int extraDamage)
     {
-        return DownfallHook.Aggregate<IModifyWizExtraDamage, decimal>(wizPower.CombatState,
+        return HookUtils.Aggregate<IModifyWizExtraDamage, decimal>(wizPower.CombatState,
             extraDamage, (m, k) => m.ModifyWizExtraDamage(wizPower, k));
     }
 
     public static bool ShouldGremlinSwap(ICombatState cs, Player player, Creature gremlin)
     {
-        return DownfallHook.All<IShouldGremlinSwap>(cs, m => m.ShouldGremlinSwap(player, gremlin));
+        return HookUtils.All<IShouldGremlinSwap>(cs, m => m.ShouldGremlinSwap(player, gremlin));
     }
 
     public static Task AfterWizConsumed(ICombatState cs, PlayerChoiceContext ctx, Creature oldOwner)
     {
-        return DownfallHook.Dispatch<IAfterWizConsumed>(cs, ctx,
+        return HookUtils.Dispatch<IAfterWizConsumed>(cs, ctx,
             m => m.AfterWizConsumed(ctx, oldOwner));
     }
 }
