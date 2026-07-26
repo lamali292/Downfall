48d67a73dfac795b02cfa56265465cf6431677d6
lamali <lamali292@gmail.com>
2026-07-26 21:29:11 +0200

    Remove redundant `IsAutoPlay` check in `SneckoCardEffectHandler` overflow condition.

--- SneckoCode/Core/SneckoCardEffectHandler.cs
+++ SneckoCode/Core/SneckoCardEffectHandler.cs
@@ -13,8 +13,7 @@ public static class SneckoCardEffectHandler
     {
         if (card is IHasOverflowEffect overflow
             && card.Keywords.Contains(SneckoKeywords.Overflow)
-            && SneckoCmd.OverflowActive(card)
-            && !cardPlay.IsAutoPlay)
+            && SneckoCmd.OverflowActive(card))
         {
             await overflow.OverflowEffect(ctx, cardPlay);
             await SneckoHook.AfterOverflowEffect(card.Owner, cardPlay, card);
