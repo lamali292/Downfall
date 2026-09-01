2842b6d9b6b6843ba1761c97eb505b5238bc1dac
lamali <lamali292@gmail.com>
2026-09-01 18:29:58 +0200

    feat(champ): rework Protective Aura -> Striking Aura. Whenever you play 4 "Strike" cards, gain E. (tracks between turns, like Orbit)

--- ChampCode/Powers/ProtectiveAuraPower.cs
+++ ChampCode/Powers/ProtectiveAuraPower.cs
@@ -1,21 +1,66 @@
 ﻿using Champ.ChampCode.Core;
-using Champ.ChampCode.Extensions;
-using Champ.ChampCode.Stance;
-using MegaCrit.Sts2.Core.Combat;
 using MegaCrit.Sts2.Core.Commands;
-using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Powers;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
-using MegaCrit.Sts2.Core.ValueProps;
 
 namespace Champ.ChampCode.Powers;
 
 public class ProtectiveAuraPower : ChampPowerModel
 {
-    public override async Task BeforeSideTurnEnd(PlayerChoiceContext choiceContext, CombatSide side,
-        IEnumerable<Creature> participants)
+    private  int Threshold => DynamicVars.Cards.IntValue;
+
+    public ProtectiveAuraPower()
     {
-        if (side != Owner.Side || Owner.Player == null || !Owner.Player.IsInChampStance<ChampNoStance>()) return;
-        await CreatureCmd.GainBlock(Owner, Amount, BlockProps.nonCardUnpowered, null);
-        Flash();
+        WithEnergy(1);
+        WithCards(4);
+    }
+    
+    public override PowerInstanceType InstanceType => PowerInstanceType.Instanced;
+
+    public override int DisplayAmount
+    {
+        get
+        {
+            var threshold = Threshold;
+            if (threshold <= 0) return 0;
+            return threshold - GetInternalData<Data>().StrikesPlayed % threshold;
+        }
+    }
+    protected override object InitInternalData() => new Data();
+    
+
+    public override async Task AfterCardPlayed(PlayerChoiceContext ctx, CardPlay cardPlay)
+    {
+        var card = cardPlay.Card;
+        if (card.Owner.Creature != Owner || !card.Tags.Contains(CardTag.Strike))
+            return;
+
+        var data = GetInternalData<Data>();
+        if (!data.SkippedFirst)
+        {
+            data.SkippedFirst = true;
+            return;
+        }
+
+        
+        data.StrikesPlayed++;
+
+        var triggers = data.StrikesPlayed / Threshold - data.TriggerCount;
+        if (triggers > 0)
+        {
+            Flash();
+            await PlayerCmd.GainEnergy(Amount * triggers, card.Owner);
+            data.TriggerCount += triggers;
+        }
+
+        InvokeDisplayAmountChanged();
+    }
+
+    private class Data
+    {
+        public int StrikesPlayed;
+        public int TriggerCount;
+        public bool SkippedFirst;
     }
 }
