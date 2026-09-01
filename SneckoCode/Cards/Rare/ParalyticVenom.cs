b4ae640dc11a2142be2b51038cd614d272ffde59
lamali <lamali292@gmail.com>
2026-09-01 18:21:13 +0200

    fix(snecko): replace Forked Tongue card with Paralytic Venom card and corresponding power implementation -> 4 (3) - Whenever an Attack deals unblocked damage, apply 1 Venom.

---
+++ SneckoCode/Cards/Rare/ParalyticVenom.cs
@@ -0,0 +1,25 @@
+﻿using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using Snecko.SneckoCode.Core;
+using Snecko.SneckoCode.Powers;
+
+namespace Snecko.SneckoCode.Cards.Rare;
+
+[Pool(typeof(SneckoCardPool))]
+public class ParalyticVenom : SneckoCardModel
+{
+    public ParalyticVenom() : base(4, CardType.Power, CardRarity.Rare, TargetType.Self)
+    {
+        WithCostUpgradeBy(-1);
+        WithPower<ParalyticVenomPower>(1, false);
+        WithTip<VenomPower>();
+    }
+
+    protected override async Task OnPlayInternal(PlayerChoiceContext ctx, CardPlay cardPlay)
+    {
+        await CreatureCmd.TriggerAnim(Owner.Creature, "Cast", Owner.Character.CastAnimDelay);
+        await CommonActions.ApplySelf<ParalyticVenomPower>(ctx, this);
+    }
+}
