8136878066ae76c96c1c0c0a20f5d684ca97122d
lamali <lamali292@gmail.com>
2026-09-02 18:41:58 +0200

    refactor(champ): rework "Arena Mastery" - 1(0) - uncommon - "Whenever you play 4 "Strike" cards, gain 1 energy." and "Protective Aura" - 1 - rare - "Defensive Finishers grant 3(5) additional Block.  "

--- ChampCode/Cards/Rare/ProtectiveAura.cs
+++ ChampCode/Cards/Rare/ProtectiveAura.cs
@@ -4,19 +4,17 @@ using Champ.ChampCode.Powers;
 using Downfall.DownfallCode.Artists;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
 
-namespace Champ.ChampCode.Cards.Uncommon;
+namespace Champ.ChampCode.Cards.Rare;
 
 [Pool(typeof(ChampCardPool))]
 public class ProtectiveAura : ChampCardModel
 {
-    public ProtectiveAura() : base(1, CardType.Power, CardRarity.Uncommon, TargetType.Self)
+    public ProtectiveAura() : base(1, CardType.Power, CardRarity.Rare, TargetType.Self)
     {
-        WithPower<ProtectiveAuraPower>(1, false);
-        WithCards(4);
-        WithEnergy(1);
-        WithCostUpgradeBy(-1);
-        WithTags(CardTag.Strike);
+        WithPower<ProtectiveAuraPower>(3, 2, false);
+        WithTip(StaticHoverTip.Block);
     }
 
     protected override Artist Artist => Artist.Get<GoofballMcgee>();
@@ -24,5 +22,6 @@ public class ProtectiveAura : ChampCardModel
     protected override async Task OnPlayInternal(PlayerChoiceContext ctx, CardPlay cardPlay)
     {
         await CommonActions.ApplySelf<ProtectiveAuraPower>(ctx, this);
+        
     }
 }
