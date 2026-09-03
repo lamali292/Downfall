9c92fa79897dd0dc3091ac491118c21bad0be0c1
lamali <lamali292@gmail.com>
2026-09-03 23:14:18 +0200

    feat(collector): add `Pyreblast` card with damage scaling and localization updates
    - Introduced new Rare Attack card `Pyreblast` with scaling energy cost reduction based on exhausted Status and Curse cards.
    - Added relevant localization entries for `Pyreblast` title and description.

---
+++ CollectorCode/Cards/Rare/Pyreblast.cs
@@ -0,0 +1,32 @@
+﻿using BaseLib.Abstracts;
+using BaseLib.Utils;
+using Collector.CollectorCode.Core;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models;
+
+namespace Collector.CollectorCode.Cards.Rare;
+
+[Pool(typeof(CollectorCardPool))]
+public class Pyreblast : CollectorCardModel
+{
+    public Pyreblast() : base(10, CardType.Attack, CardRarity.Rare, TargetType.AnyEnemy)
+    {
+        WithKeyword(CardKeyword.Retain);
+        WithDamage(60, 40);
+        WithEnergy(1);
+        WithTip(CardKeyword.Exhaust);
+    }
+
+    public override Task AfterCardExhausted(PlayerChoiceContext ctx, CardModel card, bool causedByEthereal)
+    {
+        if (card.Owner != Owner || card.Type is not (CardType.Curse or CardType.Status) || Pile is not { Type: PileType.Hand }) return Task.CompletedTask;
+        EnergyCost.AddThisCombat(-DynamicVars.Energy.IntValue);
+        return Task.CompletedTask;
+    }
+
+    protected override async Task OnPlayInternal(PlayerChoiceContext ctx, CardPlay cardPlay)
+    {
+        await CommonActions.CardAttack(this, cardPlay).Execute(ctx);
+    }
+}
