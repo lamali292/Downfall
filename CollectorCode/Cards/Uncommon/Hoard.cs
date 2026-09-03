f8b030dd0a092d4ee560d346e2366d060a9a2935
lamali <lamali292@gmail.com>
2026-09-04 00:30:19 +0200

    feat(collector): Hoard - Pyre. Draw cards until you have 6(7) in your hand.

--- CollectorCode/Cards/Uncommon/Hoard.cs
+++ CollectorCode/Cards/Uncommon/Hoard.cs
@@ -3,9 +3,11 @@ using Collector.CollectorCode.Core;
 using Collector.CollectorCode.CustomEnums;
 using Collector.CollectorCode.Interfaces;
 using Downfall.DownfallCode.Artists;
+using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Cards;
 
 namespace Collector.CollectorCode.Cards.Uncommon;
 
@@ -15,7 +17,7 @@ public class Hoard : CollectorCardModel, IHasPyre
     public Hoard() : base(1, CardType.Skill, CardRarity.Uncommon, TargetType.Self)
     {
         WithKeyword(CollectorKeyword.Pyre);
-        WithCards(2, 1);
+        WithCards(6, 1);
     }
 
     protected override Artist Artist => Artist.Get<Opal>();
@@ -24,8 +26,11 @@ public class Hoard : CollectorCardModel, IHasPyre
 
     protected override async Task OnPlayInternal(PlayerChoiceContext ctx, CardPlay cardPlay)
     {
-        var cards = await CommonActions.Draw(this, ctx);
-        foreach (var card in cards)
-            card.GiveSingleTurnRetain();
+        while (Owner.Hand.Count < DynamicVars.Cards.IntValue)
+        {
+            var drawn = await CardPileCmd.Draw(ctx, Owner);
+            if (drawn == null) break;
+        }
+
     }
 }
