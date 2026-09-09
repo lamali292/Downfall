e3f528a9545059bbc0796284e4b015942b3f18f9
lamali <lamali292@gmail.com>
2026-09-09 19:36:24 +0200

    feat(guardian): update SapphireGem rarity from Basic to Common

--- GuardianCode/Gems/SapphireGem.cs
+++ GuardianCode/Gems/SapphireGem.cs
@@ -17,7 +17,7 @@ public class SapphireGem : GemModel
     public override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.Static(GuardianTip.Brace)];
     protected override IEnumerable<DynamicVar> CanonicalVars => [new GemVar(5)];
     public override Color GemColor => new(0x0624BEFF);
-    public override CardRarity Rarity => CardRarity.Basic;
+    public override CardRarity Rarity => CardRarity.Common;
 
     protected override async Task OnPlayInternal(PlayerChoiceContext ctx, CardPlay? cardPlay,
         IEnumerable<Player> targetPlayers)
