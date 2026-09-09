b8b562442f02146f54d6edbdb9f0bc8979f50fc1
lamali <lamali292@gmail.com>
2026-09-10 01:30:28 +0200

    feat(collector): update PhrogParasiteCard cost from 1 to 2

--- CollectorCode/Cards/Collectibles/PhrogParasiteCard.cs
+++ CollectorCode/Cards/Collectibles/PhrogParasiteCard.cs
@@ -9,7 +9,7 @@ namespace Collector.CollectorCode.Cards.Collectibles;
 
 public class PhrogParasiteCard : Collectible<PhrogParasiteElite>
 {
-    public PhrogParasiteCard() : base(1, CardType.Skill, CardRarity.Uncommon, TargetType.AnyEnemy, 0.4f)
+    public PhrogParasiteCard() : base(2, CardType.Skill, CardRarity.Uncommon, TargetType.AnyEnemy, 0.4f)
     {
         WithBlock(19, 4);//Keep in mind manifest is a common.
         WithCardTip<Infection>();
