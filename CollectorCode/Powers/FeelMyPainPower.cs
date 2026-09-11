f0fa8cb914a20d28b21b6779e2b1cad480535e4d
lamali <lamali292@gmail.com>
2026-09-11 02:32:14 +0200

    fix(collector): correct "Feel Our Pain" trigger and update localization
    - Adjusted power logic to trigger only when the player plays a Colorless card.
    - Updated card and power descriptions in localization files for consistency.

--- CollectorCode/Powers/FeelMyPainPower.cs
+++ CollectorCode/Powers/FeelMyPainPower.cs
@@ -3,36 +3,25 @@ using Collector.CollectorCode.CustomEnums;
 using Collector.CollectorCode.Events;
 using Downfall.DownfallCode.Compatibility;
 using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.ValueProps;
 
 namespace Collector.CollectorCode.Powers;
 
-public class FeelMyPainPower : CollectorPowerModel, IAfterCardPyred
+public class FeelMyPainPower : CollectorPowerModel
 {
-    public FeelMyPainPower()
+    public override async Task AfterCardPlayed(PlayerChoiceContext ctx, CardPlay cardPlay)
     {
-        WithTip(CollectorTip.Pyred);
-    }
-    
-    
-    public async Task AfterCardPyred(PlayerChoiceContext ctx, CardModel card, CardModel pyred)
-    {
-        if (pyred.Owner.Creature != Owner) return;
-        //var creature = CombatState.RunState.Rng.CombatTargets.NextItem(CombatState.HittableEnemies);
-        //if (creature == null) return;
-        //await CompatibilityCreatureCmd.Damage(ctx, creature, Amount, DamageProps.nonCardHpLoss, Owner, null, null);
-        var ctxy = new BlockingPlayerChoiceContext();
+        var card = cardPlay.Card;
+        if (card.Owner.Creature != Owner || !card.VisualCardPool.IsColorless) return;
         Flash();
-        var currentEnemies = CombatState.Enemies.ToList();
-        foreach (var enemy in currentEnemies)//Same as SongOfSorrowPower
-            if (enemy is { IsHittable: true, IsAlive: true })
-                await CreatureCmd.Damage(ctxy,
-                    enemy,
-                    Amount,
-                    DamageProps.nonCardHpLoss,
-                    Owner);
-        Flash();
+        await CreatureCmd.Damage(ctx,
+            CombatState.HittableEnemies,
+            Amount,
+            DamageProps.nonCardHpLoss,
+            Owner);
+  
     }
 }
