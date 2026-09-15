0a0085a986ae7809c83cef2983371742d37a2b43
SandTag <122702937+SandTag@users.noreply.github.com>
2026-09-15 21:35:37 +0100

    Return to hand thingy

--- CollectorCode/Cards/Rare/SlagTeam.cs
+++ CollectorCode/Cards/Rare/SlagTeam.cs
@@ -1,19 +1,24 @@
 ﻿using BaseLib.Utils;
+using Collector.CollectorCode.Cards.Token;
 using Collector.CollectorCode.Core;
+using Collector.CollectorCode.Events;
 using Collector.CollectorCode.Extensions;
 using Downfall.DownfallCode.Artists;
+using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Localization;
+using MegaCrit.Sts2.Core.Models;
 
 namespace Collector.CollectorCode.Cards.Rare;
 
 [Pool(typeof(CollectorCardPool))]
-public class SlagTeam : CollectorCardModel
+public class SlagTeam : CollectorCardModel, IAfterCardPyred
 {
     public SlagTeam() : base(0, CardType.Attack, CardRarity.Rare, TargetType.AnyEnemy)
     {
         WithTorchheadDamage(6, 3);
+        WithTip<Ember>();
     }
     
     protected override bool ShouldGlowRedInternal => Owner.IsTorchheadMissing;
@@ -23,6 +28,19 @@ public class SlagTeam : CollectorCardModel
     protected override async Task OnPlayInternal(PlayerChoiceContext ctx, CardPlay cardPlay)
     {
         await CollectorCmd.TorchheadAttack(this, cardPlay).ExecuteIfPresent(ctx);
+    }
+
+    public async Task AfterCardPyred(PlayerChoiceContext ctx, CardModel card, CardModel pyred)
+    {
+        if (pyred is not Ember)
+        {
+            return;
+        }
+        if (Pile == null || Pile.Type == PileType.Hand)
+        {
+            return;
+        }
+        CardPileAddResult cardPileAddResult = await CardPileCmd.Add(this, PileType.Hand);
     }
 
     protected override void AddExtraArgsToDescription(LocString description)
