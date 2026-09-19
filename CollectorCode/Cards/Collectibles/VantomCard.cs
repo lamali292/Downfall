8c70ead7f382c2047791e91a0d2b5964a4aab6f4
SandTag <122702937+SandTag@users.noreply.github.com>
2026-09-19 22:48:49 +0100

    Slippery

--- CollectorCode/Cards/Collectibles/VantomCard.cs
+++ CollectorCode/Cards/Collectibles/VantomCard.cs
@@ -1,5 +1,6 @@
 ﻿using BaseLib.Utils;
 using Collector.CollectorCode.Cards.Token;
+using Collector.CollectorCode.Powers;
 using Hermit.HermitCode.Powers;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
@@ -8,13 +9,14 @@ namespace Collector.CollectorCode.Cards.Collectibles;
 
 public class VantomCard : Collectible<VantomBoss>
 {
-    public VantomCard() : base(2, CardType.Power, CardRarity.Rare, TargetType.Self, 0.67f)
+    public VantomCard() : base(3, CardType.Power, CardRarity.Rare, TargetType.Self, 0.67f)
     {
-        WithPower<RuggedPower>(2, 1);
+        WithPower<PlayerSlipperyPower>(2);
+        WithCostUpgradeBy(-1);
     }
 
     protected override async Task OnPlayInternal(PlayerChoiceContext ctx, CardPlay cardPlay)
     {
-        await CommonActions.ApplySelf<RuggedPower>(ctx, this);
+        await CommonActions.ApplySelf<PlayerSlipperyPower>(ctx, this);
     }
 }
---
+++ CollectorCode/Powers/PlayerSlipperyPower.cs
@@ -0,0 +1,26 @@
+﻿using Collector.CollectorCode.Core;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace Collector.CollectorCode.Powers;
+
+public class PlayerSlipperyPower : CollectorPowerModel
+{
+    
+    public override decimal ModifyHpLostBeforeOsty(Creature target, decimal amount, ValueProp props, Creature? dealer,
+        CardModel? cardSource)
+    {
+        if (target != Owner || Amount <= 0 || amount <= 0m)
+            return amount;
+
+        return Math.Min(amount, 1m);
+    }
+
+    public override Task AfterModifyingHpLostBeforeOsty()
+    {
+        PowerCmd.Decrement(this);
+        return Task.CompletedTask;
+    }
+}
